f356eded82795edcc7a000123fcc500d6624d12e
Doug Larrick <doug@parkercat.org>
2021-06-19 11:39:19 -0400

    Bump to 0.1.12: get_current_sensor_temperature

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pykumo",
-    version="0.1.11",
+    version="0.1.12",
     author="Doug Larrick",
     author_email="doug@parkercat.org",
     description="Small library for interfacing with Mitsubishi KumoCloud enabled devices",
