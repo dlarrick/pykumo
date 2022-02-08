6aea5f2ad206cc22ad699be4f3f7bebd6fa561a9
Brendan Davis <brgaulin@gmail.com>
2022-02-07 19:56:49 -0500

    Version bump to 0.2.1

--- pykumo/__init__.py
+++ pykumo/__init__.py
@@ -1,7 +1,7 @@
 """ Module to interact with Mitsubishi KumoCloud devices via their local API.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 name = "pykumo"
 
 from .py_kumo_cloud_account import KumoCloudAccount
