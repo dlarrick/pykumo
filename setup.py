27e33cba8279dbd1744c67072e3375d05851b3fb
Doug Larrick <doug@parkercat.org>
2023-03-05 11:54:31 -0500

    Issue 35 related: stop early fetching sensor data on error Should be slightly faster with no sensors.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pykumo",
-    version="0.3.2",
+    version="0.3.3",
     author="Doug Larrick",
     author_email="doug@parkercat.org",
     description="Small library for interfacing with Mitsubishi KumoCloud enabled devices",
