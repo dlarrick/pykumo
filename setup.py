282ac1f9b744327a29214bae65ba8e1ec4efb55f
Doug Larrick <doug@parkercat.org>
2023-07-03 13:35:56 -0400

    setup.py typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dlarrick/pykumo",
     packages=setuptools.find_packages(),
-    install_requires=['requests', 'urllib3']
+    install_requires=['requests', 'urllib3'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
