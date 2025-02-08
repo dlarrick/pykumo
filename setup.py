17cd8b07733d236251882a2c1d527475b9a1fd1f
dlarrick <doug@parkercat.org>
2025-02-08 11:55:01 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pykumo",
-    version="0.3.9",
+    version="0.3.10",
     author="Doug Larrick",
     author_email="doug@parkercat.org",
     description="Small library for interfacing with Mitsubishi KumoCloud enabled devices",
