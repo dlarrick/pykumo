f3affe6490867042eafce69c66614e20d34205a5
dlarrick <doug@parkercat.org>
2026-02-28 14:11:33 -0500

    Prep for 0.4.0 release (#60)

--- pykumo/__init__.py
+++ pykumo/__init__.py
@@ -1,7 +1,7 @@
 """ Module to interact with Mitsubishi KumoCloud devices via their local API.
 """
 
-__version__ = '0.3.11'
+__version__ = '0.4.0'
 name = "pykumo"
 
 from .py_kumo_cloud_account import KumoCloudAccount
