62a389ff6ed565b1bb58181b29d81c61ce32b040
dlarrick <doug@parkercat.org>
2026-05-02 08:16:36 -0400

    Prep for v0.4.2 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pykumo",
-    version="0.4.1",
+    version="0.4.2",
     author="Doug Larrick",
     author_email="doug@parkercat.org",
     description="Small library for interfacing with Mitsubishi KumoCloud enabled devices",
